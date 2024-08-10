4f28e74313fef08427a2acd94b683bb00f03c1df
Anshul Agrawal <95462842+anshulagrawal45@users.noreply.github.com>
2024-08-10 12:49:00 +0530

    Delete Features directory

--- Features/StaticPages/LoadingPage.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, CircularProgress, Typography } from "@mui/material";
-
-export default function LoadingPage(){
-    return <Box height={"100vh"} justifyContent={"center"} display={"flex"} flexDirection={"column"} alignItems={"center"}>
-    <Typography variant="h6">Loading, Please Wait...</Typography>
-    <br />
-    <CircularProgress size={"100px"} color="secondary"  />
-    </Box>
-}
